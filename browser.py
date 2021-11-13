4578a5981c0450fcc1abc99904a40383e7dc902c
Greg Hamel <greg.hamel@beemhq.com>
2021-11-13 12:14:52 -0500

    feat(renderer): render correctly spaced font, reimplement zoom and resize

--- browser.py
+++ browser.py
@@ -23,6 +23,11 @@ AMP_CHAR_CODES = {
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 
+BROWSER_MODES = {
+    "source": "source",
+    "normal": "normal"
+}
+
 redirect_counter = 0
 
 cache = Cache()
@@ -139,54 +144,113 @@ def request(url, additional_headers = {}, redirect_number = 0):
 
     return response_headers, body
 
-def lex(body):
-    in_angle = False
-    in_body_tag = False
-    possible_amp_code_index = 0
-    current_tag = ""
+class Text:
+    def __init__(self, text):
+        self.text = self.transform_amp(text)
+
+    def transform_amp(self, input_text):
+        text = ""
+        possible_amp_code_index = 0
+        for c in input_text:
+            if c == ";":
+                possible_amp_code = text[possible_amp_code_index + 1:]
+                if possible_amp_code in AMP_CHAR_CODES:
+                    # ensure we only replace last portion
+                    text = text[:possible_amp_code_index] + AMP_CHAR_CODES[possible_amp_code]
+                else:
+                    text += c
+            else:
+                if c == "&":
+                    possible_amp_code_index = len(text)
+                text += c
+        
+        return text
+
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
+def lex(body, browser_mode):
+    if browser_mode == BROWSER_MODES['source']:
+        body = body.replace("<", "&lt;").replace(">", "&gt;")
+    out = []
     text = ""
+    in_tag = False
     for c in body:
         if c == "<":
-            text = text.rstrip(' \t')
-            in_angle = True
+            in_tag = True
+            if text: out.append(Text(text))
+            text = ""
         elif c == ">":
-            in_angle = False
-            if current_tag == "body" or current_tag.startswith('body '):
-                in_body_tag = True
-            elif current_tag == "/body":
-                in_body_tag = False
-            current_tag = ""
-        elif in_angle:
-            current_tag += c
-        elif c == ";":
-            possible_amp_code = text[possible_amp_code_index + 1:]
-            if possible_amp_code in AMP_CHAR_CODES:
-                # ensure we only replace last portion
-                text = text[:possible_amp_code_index] + AMP_CHAR_CODES[possible_amp_code]
-            elif in_body_tag:
-                text += c
-        elif not in_angle and in_body_tag:
-            if c == "&":
-                possible_amp_code_index = len(text) 
+            in_tag = False
+            out.append(Tag(text))
+            text = ""
+        else:
             text += c
+    if not in_tag and text:
+        out.append(Text(text))
 
-    return text
+    if browser_mode == BROWSER_MODES['normal']:
+        body_start = 0
+        body_end = -1
 
-def layout(text, width=WIDTH, h_step=HSTEP, v_step=VSTEP):
-    display_list = []
-    cursor_x, cursor_y = h_step, v_step
-    for c in text:
-        if c == '\n':
-            cursor_x = h_step
-            cursor_y += 1.3 * v_step
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += h_step
-        if cursor_x >= width - h_step:
-            cursor_y += v_step
-            cursor_x = h_step
+        for index, token in enumerate(out):
+            if isinstance(token, Tag):
+                if token.tag.startswith('body'):
+                    body_start = index + 1
+                elif token.tag.startswith('/body'):
+                    body_end = index
+        
+        return out[body_start : body_end]
+
+    return out
+
+class Layout:
+    def __init__(self, tokens, font, canvas_width):
+        self.display_list = []
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = font.actual('weight')
+        self.slant = font.actual('slant')
+        self.size = font.actual('size')
+        self.family = font.actual('family')
+        self.canvas_width = canvas_width
+
+        for token in tokens:
+            self.tokenize(token)
+
+    def tokenize(self, token):
+        if isinstance(token, Text):
+            self.text(token)
+        if isinstance(token, Tag):
+            self.tag(token)
+
+    def text(self, token):
+        font = tkinter.font.Font(
+            family=self.family or 'Times',
+            size=self.size,
+            weight=self.weight,
+            slant=self.slant,
+        )
+        for word in token.text.split():
+            w = font.measure(word)
+            if self.cursor_x + w > self.canvas_width - HSTEP:
+                self.cursor_y += font.metrics("linespace") * 1.25
+                self.cursor_x = HSTEP
+            self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+            self.cursor_x += w + font.measure(" ")
     
-    return display_list
-
+    def tag(self, token):
+        if token.tag.startswith("i"):
+            self.slant = "italic"
+        elif token.tag.startswith("/i"):
+            self.slant = "roman"
+        elif token.tag.startswith("b"):
+            self.weight = "bold"
+        elif token.tag.startswith("/b"):
+            self.weight = "normal"
+            
 
 class Browser:
     SCROLL_STEP = 100
@@ -194,6 +258,7 @@ class Browser:
     def __init__(self):
         self.display_list = []
         self.body = ""
+        self.body_tokens = []
         self.h_step = HSTEP
         self.v_step= VSTEP
 
@@ -205,14 +270,23 @@ class Browser:
         )
         self.canvas.pack(fill='both', expand=True)
 
+        self.font = tkinter.font.Font(
+            family="Times",
+            size=16,
+            weight="normal",
+            slant="roman",
+        )
+
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
         self.window.bind("<MouseWheel>", self.mouse_scroll)
-        self.window.bind("<Configure>", self.resize)
         self.window.bind("<plus>", self.zoomin)
         self.window.bind("<minus>", self.zoomout)
+        self.window.bind("<Configure>", self.resize)
 
         self.scroll = 0
+        self.mode = BROWSER_MODES["normal"]
+        self.draw_count = 0
 
     def mouse_scroll(self, event):
         self.scroll -= event.delta * 3
@@ -222,8 +296,7 @@ class Browser:
         self.draw()
 
     def resize(self, event):
-        # self.canvas.pack(fill='both', expand=True)
-        self.display_list = layout(self.body, event.width, self.h_step, self.v_step)
+        self.display_list = Layout(self.body_tokens, self.font, event.width).display_list
         self.draw()
 
     def scrolldown(self, event):
@@ -237,44 +310,41 @@ class Browser:
         self.draw()
 
     def zoomin(self, event):
-        self.h_step, self.v_step = int(self.h_step * 1.2), int(self.v_step * 1.2)
-        self.display_list = layout(
-            self.body, 
-            self.canvas.winfo_width(), 
-            self.h_step, 
-            self.v_step
-            )
+        self.font.config(size=int(self.font.actual('size') * 1.2))
+        self.display_list = Layout(self.body_tokens, self.font, self.canvas.winfo_reqwidth()).display_list
         self.draw()
     
     def zoomout(self, event):
-        self.h_step, self.v_step = int(self.h_step / 1.2), int(self.v_step / 1.2)
-        if self.h_step < 9: self.h_step = 9
-        if self.v_step < 9: self.v_step = 9
-        self.display_list = layout(
-            self.body,
-            self.canvas.winfo_width(), 
-            self.h_step, 
-            self.v_step
-            )
+        next_font_size = int(self.font.actual('size') / 1.2) if int(self.font.actual('size') / 1.2) > 9 else 9
+        self.font.config(size=next_font_size)
+        self.display_list = Layout(
+            self.body_tokens,
+            self.font,
+            self.canvas.winfo_reqwidth()
+            ).display_list
         self.draw()
 
     def draw(self):
         self.canvas.delete("all")
-        font = tkinter.font.Font(size=self.v_step)
-        for x, y, c in self.display_list:
-            if y > self.scroll + self.canvas.winfo_height(): continue # below view
-            if y + VSTEP < self.scroll: continue # above view
-            self.canvas.create_text(x, y - self.scroll, text=c, font=font)
+        for x, y, c, font in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue # below view
+            if y + font.metrics('linespace') < self.scroll: 
+                continue # above view
+            self.canvas.create_text(x, y - self.scroll, text=c, font=font, anchor="nw")
 
     def load(self, url):
         if url.startswith('view-source:'):
+            self.mode = BROWSER_MODES["source"]
             _, url = url.split(":", 1)
             headers, body = request(url)
-            body = "<body>" + body.replace("<", "&lt;").replace(">", "&gt;") + "</body>" 
+            self.body_tokens = lex(body, self.mode)
+            self.display_list = Layout(self.body_tokens, self.font, self.canvas.winfo_reqwidth()).display_list
         else:
+            self.mode = BROWSER_MODES["normal"]
             headers, body = request(url)
-        self.body = lex(body)
-        self.display_list = layout(self.body, self.window.winfo_width(), self.h_step, self.v_step)
+            self.body_tokens = lex(body, self.mode)
+            self.display_list = Layout(self.body_tokens, self.font, self.canvas.winfo_reqwidth()).display_list
         self.draw()
 
 if __name__ == "__main__":
